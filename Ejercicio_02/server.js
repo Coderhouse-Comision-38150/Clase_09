5bd4a1fde43cf98aef74c44db38ef8171e13808b
jpromanonet <programming@jpromano.net>
2022-12-07 21:31:03 -0300

    Formulario para cargar datos

---
+++ Ejercicio_02/server.js
@@ -0,0 +1,52 @@
+/* Importar librerias que necesitemos */
+
+import express from "express"
+
+/* Crear constantes para el router y sus estados */
+
+const app = express();
+const routerMascotas = express.Router();
+const routerPersonas = express.Router();
+
+routerMascotas.use(express.json());
+routerPersonas.use(express.json());
+routerMascotas.use(express.urlencoded({extended: true}));
+routerPersonas.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+/* Mascotas */
+
+const mascotas = []
+
+routerMascotas.get('/listar', (req, res) => {
+    res.json(mascotas);
+})
+
+routerMascotas.post('/guardar', (req, res) => {
+    mascotas.push(req.body)
+    res.json(req.body)
+})
+
+/* Personas */
+
+const personas = [];
+
+routerPersonas.get('/listar', (req, res) => {
+    res.json(personas)
+})
+
+routerPersonas.post('/guardar', (req, res) => {
+    personas.push(req.body)
+    res.json(req.body)
+})
+
+/* ------------------------------------------------------ */
+
+/* Setup del servidor */
+
+const PORT = 8080;
+const server = app.listen(PORT, () =>{
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
